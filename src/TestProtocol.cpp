b6014c628c2f454e02936c20736b9d79872a5873
Khachatur Avetisyan <72801992+KhachaturAvetisyan@users.noreply.github.com>
2024-03-07 19:03:03 +0400

    add DEBUG define and test protocol

--- src/TestProtocol.cpp
+++ src/TestProtocol.cpp
@@ -1,5 +1,7 @@
 #include "TestProtocol.hpp"
 
+#include <unistd.h>
+
 TestProtocol::TestProtocol()
 {
     // TestProtocol arguments initialization
@@ -10,5 +12,30 @@ void TestProtocol::handler_loop(int _socket_fd)
     socket_fd = _socket_fd;
 
     // TestProtocol handler loop
+    std::array<uint8_t, 256> buffer{"hello world"};
+
+    try
+    {
+        send_data(buffer.data(), buffer.size());
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "send error: " << e.what() << std::endl;
+        return;
+    }
+
+    sleep(5);
+
+    try
+    {
+        recv_data(buffer.data(), buffer.size());
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "recv error: " << e.what() << std::endl;
+        return;
+    }
+
+
 
 }
